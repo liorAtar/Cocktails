920122e4ba124d6eae95e73068386528405e2f38
lior atar <lioratar@h-MacBook-Pro-sl-lior-2.local>
2021-10-29 17:32:28 +0300

    added search

---
+++ src/components/Search.vue
@@ -0,0 +1,64 @@
+<template>
+  <nav class="black">
+    <div class="nav-wrapper">
+      <form>
+        <div class="input-field">
+          <input
+            id="search"
+            v-on:keyup="serachClicked"
+            v-model="searchtext"
+            type="search"
+            required
+          />
+          <label class="label-icon" for="search"
+            ><i class="material-icons">search</i></label
+          >
+          <i class="material-icons">close</i>
+        </div>
+      </form>
+    </div>
+  </nav>
+</template>
+
+<script>
+import M from "materialize-css";
+
+export default {
+  name: "searchtext",
+  mounted() {
+    M.AutoInit();
+  },
+  props: ["currentListForSearch"],
+  data() {
+    return {
+      searchtext: "",
+      filteredList: [],
+    };
+  },
+  watch: {
+    // Reset the list to serach in
+    searchtext: function () {
+      this.filteredList = this.currentListForSearch;
+    },
+  },
+  computed: {
+    // Filter list according to serach
+    filteredDrinks: function () {
+      return this.filteredList.filter((drink) => {
+        return drink.strDrink
+          .toLowerCase()
+          .match(this.searchtext.toLowerCase());
+      });
+    },
+  },
+  methods: {
+    serachClicked() {
+      this.$emit("serachClicked", this.filteredDrinks);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
